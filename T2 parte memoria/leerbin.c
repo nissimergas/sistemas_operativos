808835007b31e0d180e946d8f074e1ddfaa03f2f
Nissim Ergas <naergas@uc.cl>
2018-04-27 13:07:01 -0300

    asA

--- T2 parte memoria/leerbin.c
+++ T2 parte memoria/leerbin.c
@@ -4,12 +4,12 @@
 void leer_disco(int direccion,int *recuperar_disco);
 int main(){
 int *recuperar_disco=calloc(256,sizeof(int));
-//leer_disco(5,recuperar_disco);
+leer_disco(10105,recuperar_disco);
 int i=0;
 for(i=0;i<256;i++){
   //printf(" valor: %i \n", recuperar_disco[i]);
 }
-leer_disco(26843545,recuperar_disco);
+//leer_disco(26843545,recuperar_disco);
  i=0;
 for(i=0;i<256;i++){
  //printf(" valor: %i \n", recuperar_disco[i]);
@@ -22,17 +22,22 @@ return 0;
 void leer_disco(int direccion,int *recuperar_disco){
   direccion=direccion-(direccion%256);
   printf("direccion: %i",direccion );
-  unsigned char buffer[1];
+  unsigned char* buffer=calloc(256,sizeof(char));
   FILE *fp;
   fp = fopen("data.bin", "r");
   int i=0;
   fseek(fp, direccion, SEEK_SET);
+  //fread(buffer, sizeof(buffer), 1, fp);
+  //fread(buffer, sizeof(buffer), 256, fp);
   for(i=0;i<256;i++){
+  fseek(fp, direccion+i, SEEK_SET);
+  fread(buffer, sizeof(buffer), 1, fp);
     //fseek(fp, direccion, SEEK_SET);
-    fread(buffer, sizeof(buffer), 1, fp);
-    int a=*buffer;
-   printf("e %u ue\n", *buffer);
+    //fread(buffer, sizeof(buffer), 256, fp);
+    int a=buffer[0];
+   printf("%direccion i :%i\n",direccion+i, a);
     recuperar_disco[i]=a;
   }
+  free(buffer);
   fclose(fp);
 }
